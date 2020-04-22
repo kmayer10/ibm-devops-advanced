456650a19e6979d9fd85fd15446540bbf17f8f3c
Kul <kulbhushan.mayer@thinknyx.com>
2020-04-22 12:33:22 +0530

    procured new instance

--- aws.tf
+++ aws.tf
@@ -2,3 +2,13 @@
 provider "aws" {
 	region = "us-east-1"
 }
+
+# Creating CENTOS Server
+resource "aws_instance" "centos" {
+	ami           = "ami-030ff268bd7b4e8b5"
+	instance_type = "t2.micro"
+	count = 1
+	tags = {
+		Name = "Terraform-Demo"
+	}
+}
