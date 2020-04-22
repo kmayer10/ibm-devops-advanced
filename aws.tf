e2f9ef4283403912ddbf91ea4a026e6f2aedb4b8
Kul <kulbhushan.mayer@thinknyx.com>
2020-04-22 12:58:00 +0530

    created security group & added the same with EC2

--- aws.tf
+++ aws.tf
@@ -8,7 +8,42 @@ resource "aws_instance" "centos" {
 	ami           = "ami-030ff268bd7b4e8b5"
 	instance_type = "t2.micro"
 	count = 1
+	security_groups = [
+        aws_security_group.security_group.name
+    ]
 	tags = {
 		Name = "Terraform-Demo"
 	}
+}
+
+resource "aws_security_group" "security_group" {
+  name        = "terraform-demo"
+  description = "Allow TLS inbound traffic"
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-demo"
+  }
 }
